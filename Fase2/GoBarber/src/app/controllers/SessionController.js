f2249f29413ba8ad9e7a7f9d61517c7686b4810d
ismaesousa <ismaesousa1998@outlook.com>
2019-08-22 07:39:39 -0300

    feacture: field validation in session

--- Fase2/GoBarber/src/app/controllers/SessionController.js
+++ Fase2/GoBarber/src/app/controllers/SessionController.js
@@ -1,11 +1,26 @@
 import jwt from 'jsonwebtoken';
+// importa tudo do yup e coloca em Yup
+import * as Yup from 'yup';
+
 import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    })
+    // Verificando se os dados estão corretos de acordo com o esquema
+    if(!(await schema.isValid(req.body))){
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, password } = req.body;
+
     const user = await User.findOne({ where: { email } });
+
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
     }
