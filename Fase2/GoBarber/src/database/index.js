24f067fe40b6ad5bfc6984448199ad204838ad12
ismaesousa <ismaesousa1998@outlook.com>
2019-08-24 08:15:29 -0300

    feacture: Appointment/Agendamento

--- Fase2/GoBarber/src/database/index.js
+++ Fase2/GoBarber/src/database/index.js
@@ -6,8 +6,9 @@ import databaseConfig from '../config/database';
 // importar os models
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
@@ -18,9 +19,9 @@ class Database {
     this.connection = new Sequelize(databaseConfig);
     // Pecorre cada model, chamando o método init do model e passando a conexão para ele
     models
-    .map(model => model.init(this.connection))
-    // Chama o método assiciate caso exista passando todos os models
-    .map(model => model.associate && model.associate(this.connection.models));
+      .map(model => model.init(this.connection))
+      // Chama o método assiciate caso exista passando todos os models
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
--- Fase2/GoBarber/src/routes.js
+++ Fase2/GoBarber/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionsController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,7 +21,10 @@ routes.post('/users', UserController.store);
 // Somente rotas abaixo serão necessária autorização
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 // middleware upload que retorna o file
 routes.post('/files', upload.single('file'), FileController.store);
