01a38a2e97367f1432fd8d6753a732e5e037f6ef
ismaesousa <ismaesousa1998@outlook.com>
2019-08-25 12:02:36 -0300

    Feacture: sending emal

---
+++ Fase2/GoBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'e81eec7349a3a4',
+    pass: 'c0b7f280566adc',
+  },
+  default: {
+    from: 'Equipe GoBarbe <noreply@gobarber.com>',
+  },
+};
