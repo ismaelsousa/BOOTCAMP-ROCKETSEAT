5914b504b020ab090b0608e1d64d43715e3ba066
ismaesousa <ismaesousa1998@outlook.com>
2019-08-26 20:42:25 -0300

    Feacture: queue add

---
+++ Fase2/GoBarber/src/lib/Queue.js
@@ -0,0 +1,40 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+    this.init();
+  }
+
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+      bee.on('failed', this.handleFailed).process(handle);
+    });
+  }
+
+  handleFailed(job, err) {
+    console.log(`Queue ${job.queue.name}: Failed`, err);
+  }
+}
+
+export default new Queue();
