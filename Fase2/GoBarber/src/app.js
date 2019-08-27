3aff47f01957cf1b142e0615a54d355b4f609c06
ismaesousa <ismaesousa1998@outlook.com>
2019-08-27 10:55:59 -0300

    Feacture: error log with a sentry

--- Fase2/GoBarber/src/app.js
+++ Fase2/GoBarber/src/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+import sentryConfig from './config/sentry';
 import routes from './routes';
 // Chama o arq index.js e já roda automáticamente
 import './database';
@@ -7,11 +11,15 @@ import './database';
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -21,6 +29,14 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ Fase2/GoBarber/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://d6004818eccc4da8b9b2ccb439aaee71@sentry.io/1543146',
+};
