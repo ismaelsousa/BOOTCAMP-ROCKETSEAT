48937649a79320875ffbfd4d17643f3a44a65b8a
ismaesousa <ismaesousa1998@outlook.com>
2019-09-05 21:52:15 -0300

    facture: Add state and search github api

--- Fase3/react-native/firstapp/src/pages/Main/index.js
+++ Fase3/react-native/firstapp/src/pages/Main/index.js
@@ -1,23 +1,54 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Keyboard } from 'react-native';
+import api from '../../services/api'
 import { Container, Form, Input, SubmitButton } from './styles';
 import Icon from 'react-native-vector-icons/MaterialIcons'
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name='add' size={20} color="#fff"/>
-        </SubmitButton>
-      </Form>
+export default class Main extends Component {
+  state= {
+    newUser: '',
+    users: [],
+  };
 
-    </Container>
-  );
+  handleAddUser =async  ()=>{
+    const {users, newUser} =this.state
+    const response = await api.get(`/users/${newUser}`)
+    const data ={
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url
+    }
+
+    console.log(data)
+    this.setState({
+      users:[...users, data],
+      newUser:''
+    })
+
+    Keyboard.dismiss()
+  }
+  render(){
+    const {users, newUser} = this.state
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({newUser: text})}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name='add' size={20} color="#fff"/>
+          </SubmitButton>
+        </Form>
+
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
