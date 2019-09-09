b308d4a80bc98261672346f6e6e1e5c954c46692
ismaesousa <ismaesousa1998@outlook.com>
2019-09-09 13:56:57 -0300

    feature: add redux

--- 4 Fase/Arquitetura flux/rocketshoes/src/pages/Cart/index.js
+++ 4 Fase/Arquitetura flux/rocketshoes/src/pages/Cart/index.js
@@ -1,7 +1,66 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+                alt="tenis"
+              />
+            </td>
+            <td>
+              <strong>Tenis muito massa</strong>
+              <span>R$129,09</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" name="" readOnly value={1} id="" />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$122,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- 4 Fase/Arquitetura flux/rocketshoes/src/pages/Home/index.js
+++ 4 Fase/Arquitetura flux/rocketshoes/src/pages/Home/index.js
@@ -1,71 +1,46 @@
-import React from 'react';
-import { MdAddShoppingCart} from 'react-icons/md'
+import React, { Component } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
+import api from '../../services/api';
+import { formatPrice} from '../../util/format'
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+  async componentDidMount(){
+    const response = await api.get('/products');
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormated: formatPrice(product.price)
+    }))
+    this.setState({products: data})
+  }
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img src='https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x' alt='Tênis'/>
-        <strong>Tênis muito legal</strong>
-        <span>R$80,99</span>
+  render(){
 
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart size={16} color='#fff'/> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
+    const { products } = this.state
 
-      <li>
-        <img src='https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x' alt='Tênis'/>
-        <strong>Tênis muito legal</strong>
-        <span>R$80,99</span>
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img
+              src={product.image}
+              alt={product.title}
+            />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormated}</span>
 
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart size={16} color='#fff'/> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img src='https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x' alt='Tênis'/>
-        <strong>Tênis muito legal</strong>
-        <span>R$80,99</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
 
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart size={16} color='#fff'/> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img src='https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x' alt='Tênis'/>
-        <strong>Tênis muito legal</strong>
-        <span>R$80,99</span>
-
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart size={16} color='#fff'/> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x" alt="Tênis"/>
-        <strong>Tênis muito legal</strong>
-        <span>R$80,99</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color='#fff'/> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  )
+      </ProductList>
+    );
+  }
 }
