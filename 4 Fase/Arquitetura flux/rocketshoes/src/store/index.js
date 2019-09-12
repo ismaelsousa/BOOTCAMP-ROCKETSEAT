4891e5ccb760b8273d9f22f7731fca62a78d8e23
ismaesousa <ismaesousa1998@outlook.com>
2019-09-12 20:55:12 -0300

    add reactotron-redux-saga

--- 4 Fase/Arquitetura flux/rocketshoes/src/store/index.js
+++ 4 Fase/Arquitetura flux/rocketshoes/src/store/index.js
@@ -3,17 +3,24 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+const sagaMiddleware = createSagaMiddleware({
+  sagaMonitor,
+});
 
-const enhancer = process.env.NODE_ENV === 'development'
-  ? compose(
-      console.tron.createEnhancer(),
-      applyMiddleware(sagaMiddleware)
-    )
-  : applyMiddleware(sagaMiddleware);
+const enhancer =
+  process.env.NODE_ENV === 'development'
+    ? compose(
+        console.tron.createEnhancer(),
+        applyMiddleware(sagaMiddleware)
+      )
+    : applyMiddleware(sagaMiddleware);
 
 const store = createStore(rootReducer, enhancer);
 
-sagaMiddleware.run(rootSaga)
+sagaMiddleware.run(rootSaga);
 
 export default store;
