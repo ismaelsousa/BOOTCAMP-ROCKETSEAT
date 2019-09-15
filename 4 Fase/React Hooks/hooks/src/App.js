4fed841f061389cc29651799fbfd0aca657daef7
ismaesousa <ismaesousa1998@outlook.com>
2019-09-15 13:52:35 -0300

    hooks added

--- 4 Fase/React Hooks/hooks/src/App.js
+++ 4 Fase/React Hooks/hooks/src/App.js
@@ -1,26 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
   /**
    * Desestrutura pq o useState retorna o estado e uma Função
    */
-  const [tech, setTech] = useState([
-    'React js',
-    'React native',
-    'node.js'
-  ]);
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('')
 
-  function handleAdd(){
+  /**
+   * useCallback é usado para que n seja necessário o javaScript tirar 
+   * e colocar na memória a mesma função
+   */
+  
+  const handleAdd= useCallback(()=>{
     setTech([...tech, newTech])
     setNewTech('')
-  }
+  }, [tech, newTech])
 
+  /**
+   * Para execultar somente uma vez é só colocar sem nenhuma dependencia
+   */
+
+   useEffect(()=>{
+     const storage = localStorage.getItem('tech')
+     if(storage){
+       setTech(JSON.parse(storage))
+     }
+     /**
+      * Quando o component for deletado basta retornar a função
+      */
+     return ()=>{};
+   },[])
+
+  /**
+   * Recebe uma função e um array das dependencias que serão observadas
+   */
+  useEffect(()=>{
+    localStorage.setItem('tech', JSON.stringify(tech))
+  },[tech])
+
+  /**
+   * Cria uma variavel que só irá mudar quando o tech mudar
+   */
+  const techSize = useMemo(()=>tech.length, [tech])
   return (
     <>
       <ul>
         {tech.map(t=> <li key={t}>{t}</li>)}
       </ul>
+      <strong>Você tem {techSize} tecnologia</strong>
+      <br/>
       <input type="text" onChange={e => setNewTech(e.target.value) } name="" value={newTech} id=""/>
       <button type='button' onClick={handleAdd}>Adicionar </button>
     </>
