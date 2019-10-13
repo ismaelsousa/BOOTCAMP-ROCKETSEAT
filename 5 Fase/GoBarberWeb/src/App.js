cd6ffbb0964500209ea5b20748f571805a5ac0cb
ismaesousa <ismaesousa1998@outlook.com>
2019-10-13 18:36:15 -0300

    redux add

--- 5 Fase/GoBarberWeb/src/App.js
+++ 5 Fase/GoBarberWeb/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-
 import './config/ReactotronConfig';
+
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 import history from './services/history';
 
+// Sempre import o reduce depois do reactotron
+import store from './store';
+
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle/>
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
---
+++ 5 Fase/GoBarberWeb/src/store/modules/auth/actions.js
@@ -0,0 +1,22 @@
+export function signInRequest(email, password){
+  return {
+    type: '@auth/SING_IN_REQUEST',
+    payload: { email, password },
+  }
+}
+
+export function signInSuccess(token, user){
+  return {
+    type: '@auth/SIGN_IN_SUCCESS',
+    payload:{
+      token,
+      user,
+    }
+  }
+}
+
+export function signFailure(){
+  return {
+    type: '@auth/SIGN_FAILURE',
+  }
+}
