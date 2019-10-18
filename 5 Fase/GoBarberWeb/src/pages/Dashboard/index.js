3b7cf606fb5551dbb3773aaf2f1cb15dc65a7e6e
ismaesousa <ismaesousa1998@outlook.com>
2019-10-18 09:28:01 -0300

    select day

--- 5 Fase/GoBarberWeb/src/pages/Dashboard/index.js
+++ 5 Fase/GoBarberWeb/src/pages/Dashboard/index.js
@@ -1,9 +1,55 @@
-import React from 'react';
-import api from '~/services/api';
+import React,{useState, useMemo} from 'react';
+import { format,subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+import { MdChevronLeft, MdChevronRight} from 'react-icons/md';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return (<h1>Dashboard</h1>);
+  const [date, setDate]= useState(new Date());
+
+  const dateFormatted = useMemo(()=> format(date,"d 'de' MMMM", {locale:pt}),[
+    date
+  ])
+
+  function handlePrevDay(){
+    setDate(subDays(date,1));
+  }
+
+  function handleNextDay(){
+    setDate(addDays(date,1));
+  }
+  return (
+    <Container>
+      <header>
+        <button type='button' onClick={handlePrevDay}>
+          <MdChevronLeft size={36} color='#fff'/>
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type='button' onClick={handleNextDay}>
+          <MdChevronRight size={36} color='#fff'/>
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Ismael sousa</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Ismael sousa</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Ismael sousa</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
