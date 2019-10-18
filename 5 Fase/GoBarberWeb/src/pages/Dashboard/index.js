6e4e233f5ca65cc7dc2ffe5a5efface7ea09e205
ismaesousa <ismaesousa1998@outlook.com>
2019-10-18 14:25:34 -0300

    finish application

--- 5 Fase/GoBarberWeb/src/pages/Dashboard/index.js
+++ 5 Fase/GoBarberWeb/src/pages/Dashboard/index.js
@@ -1,54 +1,71 @@
-import React,{useState, useMemo} from 'react';
-import { format,subDays, addDays } from 'date-fns';
+import React, { useState, useMemo, useEffect } from 'react';
+import { format, subDays, addDays, setHours,setMinutes, setSeconds, isBefore, isEqual, parseISO } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import {utcToZonedTime} from 'date-fns-tz'
 
-import api from '~/services/api';
 import { MdChevronLeft, MdChevronRight} from 'react-icons/md';
+import api from '~/services/api';
 import { Container, Time } from './styles';
 
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
 export default function Dashboard() {
-  const [date, setDate]= useState(new Date());
+  const [date, setDate] = useState(new Date());
+  const [schedule, setSchedule] = useState([]);
 
-  const dateFormatted = useMemo(()=> format(date,"d 'de' MMMM", {locale:pt}),[
-    date
-  ])
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
 
-  function handlePrevDay(){
-    setDate(subDays(date,1));
+  useEffect(()=>{
+    async function loadSchedule(){
+      const response  = await api.get('schedule', {
+        params: {date}
+      })
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timezone
+      const data = range.map(hour => {
+        const checkDate = setSeconds(setMinutes(setHours(date, hour),0),0);
+
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        return {
+          time: `${hour}:00h`,
+          past:isBefore(compareDate, new Date()),
+          appointment: response.data.find(a=> isEqual(parseISO(a.date), compareDate) )
+        }
+      });
+      setSchedule(data);
+    }
+    loadSchedule();
+  },[date])
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
   }
 
-  function handleNextDay(){
-    setDate(addDays(date,1));
+  function handleNextDay() {
+    setDate(addDays(date, 1));
   }
   return (
     <Container>
       <header>
-        <button type='button' onClick={handlePrevDay}>
-          <MdChevronLeft size={36} color='#fff'/>
+        <button type="button" onClick={handlePrevDay}>
+          <MdChevronLeft size={36} color="#fff" />
         </button>
         <strong>{dateFormatted}</strong>
-        <button type='button' onClick={handleNextDay}>
-          <MdChevronRight size={36} color='#fff'/>
+        <button type="button" onClick={handleNextDay}>
+          <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
 
       <ul>
-        <Time past>
-          <strong>08:00</strong>
-          <span>Ismael sousa</span>
-        </Time>
-        <Time available>
-          <strong>09:00</strong>
-          <span>Em aberto</span>
-        </Time>
-        <Time>
-          <strong>10:00</strong>
-          <span>Ismael sousa</span>
-        </Time>
-        <Time>
-          <strong>11:00</strong>
-          <span>Ismael sousa</span>
-        </Time>
+        {schedule.map(time => (
+          <Time  key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>{time.appointment? time.appointment.user.name:'Em aberto'}</span>
+          </Time>
+        ))}
       </ul>
     </Container>
   );
